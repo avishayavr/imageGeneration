39202d109d3330363a8852da2ee7a444a96aeae0
avishay avraham <avishayavraham555@gmail.com>
2023-04-02 21:53:56 +0300

    home + create post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import {getRandomPrompt} from "../utils";
+import { FormField, Loader } from "../components";
+
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+    const navigate = useNavigate();
+    const [form, setForm] = useState({name:"", prompt:"", photo:"",})
+    const [generatingImg, setGenerationImg] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-export default CreatePost
+    const handleSubmit = ()=>{
+
+    }
+
+    const handleChange = (e)=>{
+
+    }
+
+    
+  return (
+    <section className="max-w-7xl mx-auto">
+         <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Create
+        </h1>
+        <p className="mt-2 text-[666e75] text-[16px] max-w-[500x]">
+          Create imaginative and visually stunning
+          images through DALL-E AI and share them with the community.
+        </p>
+      </div>
+
+      <form className="mt-16 max-w -3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+        <FormField 
+        labelName="Your name"
+        type="text"
+        name="john doe"
+        placeholder="john doe"
+        value={form.name}
+        handleChange = {handleChange}
+        />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default CreatePost;
